22f4153184a157bc29e8283d251e3ae56642c042
Asmaa Mirkhan <asmaamirkhan.am@gmail.com>
2020-02-10 19:10:18 +0300

    Seperating camerax to other activity 👮‍♀️

---
+++ app/src/main/java/com/asmaamir/mlkitdemo/CameraxActivity.java
@@ -0,0 +1,155 @@
+package com.asmaamir.mlkitdemo;
+
+import android.content.pm.PackageManager;
+import android.graphics.Matrix;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.util.Size;
+import android.view.Surface;
+import android.view.TextureView;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraX;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageAnalysisConfig;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureConfig;
+import androidx.camera.core.Preview;
+import androidx.camera.core.PreviewConfig;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.io.File;
+
+public class CameraxActivity extends AppCompatActivity {
+    private static TextureView tv;
+    private static final String TAG = "CameraxActivity";
+    public static final int REQUEST_CODE_PERMISSION = 101;
+    public static final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA", "android.permission.WRITE_EXTERNAL_STORAGE"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camerax);
+        tv = findViewById(R.id.texture_view);
+        if (allPermissionsGranted()) {
+            tv.post(this::startCamera);
+        } else {
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSION);
+        }
+        tv.addOnLayoutChangeListener((v, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom) -> {
+            updateTransform();
+        });
+    }
+
+    private void startCamera() {
+        PreviewConfig pc = new PreviewConfig
+                .Builder()
+                .setTargetResolution(new Size(tv.getWidth(), tv.getHeight()))
+                .build();
+        Preview preview = new Preview(pc);
+        preview.setOnPreviewOutputUpdateListener(output -> {
+            ViewGroup vg = (ViewGroup) tv.getParent();
+            vg.removeView(tv);
+            vg.addView(tv, 0);
+            tv.setSurfaceTexture(output.getSurfaceTexture());
+            updateTransform();
+        });
+
+        ImageCaptureConfig icc = new ImageCaptureConfig
+                .Builder()
+                .setCaptureMode(ImageCapture.CaptureMode.MAX_QUALITY)
+                .build();
+        ImageCapture imgCap = new ImageCapture(icc);
+        ImageButton ib = findViewById(R.id.img_cap);
+        ib.setOnClickListener(v -> {
+            File file = new File(Environment.getExternalStorageDirectory() + "/" + System.currentTimeMillis() + ".png");
+            imgCap.takePicture(file, (command -> {
+                command.run();
+            }), new ImageCapture.OnImageSavedListener() {
+                @Override
+                public void onImageSaved(@NonNull File file) {
+                    String msg = "Image is saved at: " + file.getAbsolutePath();
+                    runOnUiThread(() -> {
+                        Toast.makeText(getBaseContext(), msg, Toast.LENGTH_SHORT).show();
+                    });
+                    Log.i(TAG, msg);
+                }
+
+                @Override
+                public void onError(@NonNull ImageCapture.ImageCaptureError imageCaptureError, @NonNull String message, @Nullable Throwable cause) {
+                    Log.e(TAG, "An error occurred while saving:" + message);
+                }
+            });
+        });
+        ImageAnalysisConfig iac = new ImageAnalysisConfig
+                .Builder()
+                .setImageReaderMode(ImageAnalysis.ImageReaderMode.ACQUIRE_LATEST_IMAGE)
+                .build();
+
+        ImageAnalysis imageAnalysis = new ImageAnalysis(iac);
+        imageAnalysis.setAnalyzer(command -> {
+            command.run();
+        }, new DemoAnalyzer());
+
+        CameraX.bindToLifecycle(this, preview, imgCap, imageAnalysis);
+    }
+
+    private void updateTransform() {
+        Matrix mat = new Matrix();
+        float centerX = tv.getWidth() / 2.0f;
+        float centerY = tv.getHeight() / 2.0f;
+
+        float rotationDegrees;
+        switch (tv.getDisplay().getRotation()) {
+            case Surface.ROTATION_0:
+                rotationDegrees = 0;
+                break;
+            case Surface.ROTATION_90:
+                rotationDegrees = 90;
+                break;
+            case Surface.ROTATION_180:
+                rotationDegrees = 180;
+                break;
+            case Surface.ROTATION_270:
+                rotationDegrees = 270;
+                break;
+            default:
+                return;
+        }
+        mat.postRotate(rotationDegrees, centerX, centerY);
+        tv.setTransform(mat);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_PERMISSION) {
+            if (allPermissionsGranted()) {
+                tv.post(this::startCamera);
+            } else {
+                Toast.makeText(this,
+                        "Permissions not granted by the user.",
+                        Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+    private boolean allPermissionsGranted() {
+        for (String permission : REQUIRED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
--- app/src/main/java/com/asmaamir/mlkitdemo/MainActivity.java
+++ app/src/main/java/com/asmaamir/mlkitdemo/MainActivity.java
@@ -1,156 +1,64 @@
 package com.asmaamir.mlkitdemo;
 
-import android.content.pm.PackageManager;
-import android.graphics.Matrix;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.Environment;
-import android.util.Log;
-import android.util.Size;
-import android.view.Surface;
-import android.view.TextureView;
-import android.view.ViewGroup;
-import android.widget.ImageButton;
-import android.widget.Toast;
+import android.view.MenuItem;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.camera.core.CameraX;
-import androidx.camera.core.ImageAnalysis;
-import androidx.camera.core.ImageAnalysisConfig;
-import androidx.camera.core.ImageCapture;
-import androidx.camera.core.ImageCaptureConfig;
-import androidx.camera.core.Preview;
-import androidx.camera.core.PreviewConfig;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-import java.io.File;
+import com.google.android.material.navigation.NavigationView;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int REQUEST_CODE_PERMISSION = 101;
-    private static final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA", "android.permission.WRITE_EXTERNAL_STORAGE"};
-    private static TextureView tv;
     private static final String TAG = "MainActivity";
 
+    private static DrawerLayout drawerLayout;
+    private static ActionBarDrawerToggle actionBarDrawerToggle;
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv = findViewById(R.id.texture_view);
-        if (allPermissionsGranted()) {
-            tv.post(this::startCamera);
-        } else {
-            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSION);
-        }
-        tv.addOnLayoutChangeListener((v, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom) -> {
-            updateTransform();
-        });
+        initNavigationDrawer();
+    }
+
+    private void initNavigationDrawer() {
+        drawerLayout = findViewById(R.id.drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        if (getSupportActionBar() != null)
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        navigationView.setNavigationItemSelectedListener((MenuItem menuItem) -> {
+                    int id = menuItem.getItemId();
+                    switch (id) {
+                        case R.id.camerax:
+                            switchActivity(CameraxActivity.class);
+                            return true;
+                        default:
+                            return false;
+                    }
+                }
+        );
+    }
+
+    private void switchActivity(Class c) {
+        Intent intent = new Intent(this, c);
+        this.startActivity(intent);
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == REQUEST_CODE_PERMISSION) {
-            if (allPermissionsGranted()) {
-                tv.post(this::startCamera);
-            } else {
-                Toast.makeText(this,
-                        "Permissions not granted by the user.",
-                        Toast.LENGTH_SHORT).show();
-                finish();
-            }
-        }
-    }
-
-    private boolean allPermissionsGranted() {
-        for (String permission : REQUIRED_PERMISSIONS) {
-            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private void startCamera() {
-        PreviewConfig pc = new PreviewConfig
-                .Builder()
-                .setTargetResolution(new Size(tv.getWidth(), tv.getHeight()))
-                .build();
-        Preview preview = new Preview(pc);
-        preview.setOnPreviewOutputUpdateListener(output -> {
-            ViewGroup vg = (ViewGroup) tv.getParent();
-            vg.removeView(tv);
-            vg.addView(tv, 0);
-            tv.setSurfaceTexture(output.getSurfaceTexture());
-            updateTransform();
-        });
-
-        ImageCaptureConfig icc = new ImageCaptureConfig
-                .Builder()
-                .setCaptureMode(ImageCapture.CaptureMode.MAX_QUALITY)
-                .build();
-        ImageCapture imgCap = new ImageCapture(icc);
-        ImageButton ib = findViewById(R.id.img_cap);
-        ib.setOnClickListener(v -> {
-            File file = new File(Environment.getExternalStorageDirectory() + "/" + System.currentTimeMillis() + ".png");
-            imgCap.takePicture(file, (command -> {
-                command.run();
-            }), new ImageCapture.OnImageSavedListener() {
-                @Override
-                public void onImageSaved(@NonNull File file) {
-                    String msg = "Image is saved at: " + file.getAbsolutePath();
-                    runOnUiThread(() -> {
-                        Toast.makeText(getBaseContext(), msg, Toast.LENGTH_SHORT).show();
-                    });
-                    Log.i(TAG, msg);
-                }
-
-                @Override
-                public void onError(@NonNull ImageCapture.ImageCaptureError imageCaptureError, @NonNull String message, @Nullable Throwable cause) {
-                    Log.e(TAG, "An error occurred while saving:" + message);
-                }
-            });
-        });
-        ImageAnalysisConfig iac = new ImageAnalysisConfig
-                .Builder()
-                .setImageReaderMode(ImageAnalysis.ImageReaderMode.ACQUIRE_LATEST_IMAGE)
-                .build();
-
-        ImageAnalysis imageAnalysis = new ImageAnalysis(iac);
-        imageAnalysis.setAnalyzer(command -> {
-            command.run();
-        }, new DemoAnalyzer());
-
-        CameraX.bindToLifecycle(this, preview, imgCap, imageAnalysis);
-    }
-
-    private void updateTransform() {
-        Matrix mat = new Matrix();
-        float centerX = tv.getWidth() / 2.0f;
-        float centerY = tv.getHeight() / 2.0f;
-
-        float rotationDegrees;
-        switch (tv.getDisplay().getRotation()) {
-            case Surface.ROTATION_0:
-                rotationDegrees = 0;
-                break;
-            case Surface.ROTATION_90:
-                rotationDegrees = 90;
-                break;
-            case Surface.ROTATION_180:
-                rotationDegrees = 180;
-                break;
-            case Surface.ROTATION_270:
-                rotationDegrees = 270;
-                break;
-            default:
-                return;
-        }
-        mat.postRotate(rotationDegrees, centerX, centerY);
-        tv.setTransform(mat);
-
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item))
+            return true;
+        return super.onOptionsItemSelected(item);
     }
 }
