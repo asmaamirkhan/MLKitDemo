d5c5aff657e6949556eb49eb04f354d1c8a18eac
Asmaa Mirkhan <asmaamirkhan.am@gmail.com>
2020-02-15 22:44:59 +0300

    Basic detection 🙄
    Incorrect drawing 🙄

--- app/src/main/java/com/asmaamir/mlkitdemo/CaptureFaceDetection/GalleryFaceDetectionActivity.java
+++ app/src/main/java/com/asmaamir/mlkitdemo/CaptureFaceDetection/GalleryFaceDetectionActivity.java
@@ -1,49 +1,162 @@
 package com.asmaamir.mlkitdemo.CaptureFaceDetection;
 
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
-import android.view.TextureView;
+import android.util.Log;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import com.asmaamir.mlkitdemo.R;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.common.FirebaseVisionPoint;
+import com.google.firebase.ml.vision.face.FirebaseVisionFace;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceContour;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetector;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions;
+
+import java.io.IOException;
+import java.util.List;
 
 public class GalleryFaceDetectionActivity extends AppCompatActivity {
-    private TextureView tv;
-    private static final String TAG = "CameraxActivity";
+    private static final String TAG = "PickActivity";
     public static final int REQUEST_CODE_PERMISSION = 111;
     public static final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.WRITE_EXTERNAL_STORAGE", "android.permission.READ_EXTERNAL_STORAGE"};
-
+    private static final int PICK_IMAGE_CODE = 100;
+    private ImageView imageView;
+    private ImageView imageViewCanvas;
+    private ImageButton imageButton;
     private Bitmap bitmap;
     private Canvas canvas;
     private Paint dotPaint, linePaint;
-    private ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery_face_detection);
-        tv = findViewById(R.id.texture_view_capture);
-        iv = findViewById(R.id.image_view_capture);
         if (allPermissionsGranted()) {
-            tv.post(this::initDrawingUtils);
+            initViews();
         } else {
             ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSION);
         }
     }
 
+    private void initViews() {
+        imageView = findViewById(R.id.img_view_pick);
+        imageButton = findViewById(R.id.img_btn_pick);
+        imageViewCanvas = findViewById(R.id.img_view_pick_canvas);
+        imageButton.setOnClickListener(v -> {
+            pickImage();
+        });
+    }
+
+    private void pickImage() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, PICK_IMAGE_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE_CODE) {
+            imageView.setImageURI(data.getData());
+            imageViewCanvas.setImageURI(data.getData());
+            Bitmap bitmap = BitmapFactory.decodeFile(data.getData().getPath().replace("/raw/", ""));
+            Bitmap scaledImage = Bitmap.createScaledBitmap(bitmap, imageView.getWidth(), imageView.getHeight(), false);
+            //FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(scaledImage);
+            try {
+                FirebaseVisionImage image = FirebaseVisionImage.fromFilePath(this, data.getData());
+                Log.i(TAG, image.getBitmap().getWidth() + "  " + image.getBitmap().getHeight());
+                initDetector(image);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            //imageView.setImageBitmap(scaledImage);
+
+        }
+    }
+
+    private void initDetector(FirebaseVisionImage image) {
+        initDrawingUtils();
+        FirebaseVisionFaceDetectorOptions detectorOptions = new FirebaseVisionFaceDetectorOptions
+                .Builder()
+                .setContourMode(FirebaseVisionFaceDetectorOptions.ALL_CONTOURS)
+                .build();
+        FirebaseVisionFaceDetector faceDetector = FirebaseVision
+                .getInstance()
+                .getVisionFaceDetector(detectorOptions);
+        faceDetector
+                .detectInImage(image)
+                .addOnSuccessListener(firebaseVisionFaces -> {
+                    if (!firebaseVisionFaces.isEmpty()) {
+                        processFaces(firebaseVisionFaces);
+                        Log.i(TAG, "DONE");
+                    } else {
+                        canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.MULTIPLY);
+                        Log.i(TAG, "No faces");
+                    }
+                }).addOnFailureListener(e -> Log.i(TAG, e.toString()));
+    }
+
+    private void processFaces(List<FirebaseVisionFace> faces) {
+        Log.i(TAG, "Size" + faces.size());
+        for (FirebaseVisionFace face : faces) {
+            drawContours(face.getContour(FirebaseVisionFaceContour.FACE).getPoints());
+            drawContours(face.getContour(FirebaseVisionFaceContour.LEFT_EYEBROW_BOTTOM).getPoints());
+            drawContours(face.getContour(FirebaseVisionFaceContour.RIGHT_EYEBROW_BOTTOM).getPoints());
+            drawContours(face.getContour(FirebaseVisionFaceContour.LEFT_EYE).getPoints());
+            drawContours(face.getContour(FirebaseVisionFaceContour.RIGHT_EYE).getPoints());
+            drawContours(face.getContour(FirebaseVisionFaceContour.LEFT_EYEBROW_TOP).getPoints());
+            drawContours(face.getContour(FirebaseVisionFaceContour.RIGHT_EYEBROW_TOP).getPoints());
+            drawContours(face.getContour(FirebaseVisionFaceContour.LOWER_LIP_BOTTOM).getPoints());
+            drawContours(face.getContour(FirebaseVisionFaceContour.LOWER_LIP_TOP).getPoints());
+            drawContours(face.getContour(FirebaseVisionFaceContour.UPPER_LIP_BOTTOM).getPoints());
+            drawContours(face.getContour(FirebaseVisionFaceContour.UPPER_LIP_TOP).getPoints());
+            drawContours(face.getContour(FirebaseVisionFaceContour.NOSE_BRIDGE).getPoints());
+            drawContours(face.getContour(FirebaseVisionFaceContour.NOSE_BOTTOM).getPoints());
+        }
+        imageViewCanvas.setImageBitmap(bitmap);
+    }
+
+    private void drawContours(List<FirebaseVisionPoint> points) {
+        int counter = 0;
+        for (FirebaseVisionPoint point : points) {
+            if (counter != points.size() - 1) {
+                canvas.drawLine(point.getX(),
+                        point.getY(),
+                        points.get(counter + 1).getX(),
+                        points.get(counter + 1).getY(),
+                        linePaint);
+            } else {
+                canvas.drawLine(point.getX(),
+                        point.getY(),
+                        points.get(0).getX(),
+                        points.get(0).getY(),
+                        linePaint);
+            }
+            counter++;
+            canvas.drawCircle(point.getX(), point.getY(), 6, dotPaint);
+        }
+    }
 
     private void initDrawingUtils() {
-        bitmap = Bitmap.createBitmap(tv.getWidth(), tv.getHeight(), Bitmap.Config.ARGB_8888);
+        bitmap = Bitmap.createBitmap(imageView.getWidth(), imageView.getHeight(), Bitmap.Config.ARGB_8888);
         canvas = new Canvas(bitmap);
         dotPaint = new Paint();
         dotPaint.setColor(Color.RED);
@@ -62,7 +175,7 @@ public class GalleryFaceDetectionActivity extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == REQUEST_CODE_PERMISSION) {
             if (allPermissionsGranted()) {
-                tv.post(this::initDrawingUtils);
+                initViews();
             } else {
                 Toast.makeText(this,
                         "Permissions not granted by the user.",
