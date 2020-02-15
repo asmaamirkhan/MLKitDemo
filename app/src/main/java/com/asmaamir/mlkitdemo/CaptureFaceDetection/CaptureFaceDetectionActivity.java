51018803324b7d7a6def3b6be4476ab90de25ca6
Asmaa Mirkhan <asmaamirkhan.am@gmail.com>
2020-02-15 20:14:32 +0300

    Setting basic camera functionality 🤹‍♀️

--- app/src/main/java/com/asmaamir/mlkitdemo/CaptureFaceDetection/CaptureFaceDetectionActivity.java
+++ app/src/main/java/com/asmaamir/mlkitdemo/CaptureFaceDetection/CaptureFaceDetectionActivity.java
@@ -1,16 +1,135 @@
 package com.asmaamir.mlkitdemo.CaptureFaceDetection;
 
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.util.Size;
+import android.view.TextureView;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraInfoUnavailableException;
+import androidx.camera.core.CameraX;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureConfig;
+import androidx.camera.core.Preview;
+import androidx.camera.core.PreviewConfig;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.asmaamir.mlkitdemo.R;
 
+import java.io.File;
+
 public class CaptureFaceDetectionActivity extends AppCompatActivity {
+    private TextureView tv;
+    private static final String TAG = "CameraxActivity";
+    public static final int REQUEST_CODE_PERMISSION = 101;
+    public static final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA", "android.permission.WRITE_EXTERNAL_STORAGE"};
+
+    public static CameraX.LensFacing lens = CameraX.LensFacing.FRONT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_capture_face_detection);
+        tv = findViewById(R.id.texture_view_capture);
+        if (allPermissionsGranted()) {
+            tv.post(this::startCamera);
+        } else {
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSION);
+        }
+    }
+
+    @SuppressLint("RestrictedApi")
+    private void startCamera() {
+        initCamera();
+        ImageButton ibSwitch = findViewById(R.id.btn_switch_capture);
+        ibSwitch.setOnClickListener(v -> {
+            if (lens == CameraX.LensFacing.FRONT)
+                lens = CameraX.LensFacing.BACK;
+            else
+                lens = CameraX.LensFacing.FRONT;
+            try {
+                Log.i(TAG, "" + lens);
+                CameraX.getCameraWithLensFacing(lens);
+                initCamera();
+            } catch (CameraInfoUnavailableException e) {
+                Log.e(TAG, e.toString());
+            }
+        });
+    }
+
+    private void initCamera() {
+        CameraX.unbindAll();
+        PreviewConfig pc = new PreviewConfig
+                .Builder()
+                .setLensFacing(lens)
+                .setTargetResolution(new Size(tv.getWidth(), tv.getHeight()))
+                .build();
+
+        Preview preview = new Preview(pc);
+        preview.setOnPreviewOutputUpdateListener(output -> {
+            ViewGroup vg = (ViewGroup) tv.getParent();
+            vg.removeView(tv);
+            vg.addView(tv, 0);
+            tv.setSurfaceTexture(output.getSurfaceTexture());
+        });
+
+        ImageCaptureConfig icc = new ImageCaptureConfig
+                .Builder()
+                .setLensFacing(lens)
+                .setCaptureMode(ImageCapture.CaptureMode.MAX_QUALITY)
+                .build();
+
+        ImageCapture imgCap = new ImageCapture(icc);
+        ImageButton ib = findViewById(R.id.img_cap_capture);
+        ib.setOnClickListener(v -> {
+            File file = new File(Environment.getExternalStorageDirectory() + "/" + System.currentTimeMillis() + ".png");
+            imgCap.takePicture(file, (Runnable::run), new ImageCapture.OnImageSavedListener() {
+                @Override
+                public void onImageSaved(@NonNull File file) {
+                    String msg = "Image is saved at: " + file.getAbsolutePath();
+                    runOnUiThread(() -> Toast.makeText(getBaseContext(), msg, Toast.LENGTH_SHORT).show());
+                    Log.i(TAG, msg);
+                }
+
+                @Override
+                public void onError(@NonNull ImageCapture.ImageCaptureError imageCaptureError, @NonNull String message, @Nullable Throwable cause) {
+                    Log.e(TAG, "An error occurred while saving:" + message);
+                }
+            });
+        });
+        CameraX.bindToLifecycle(this, preview, imgCap);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_PERMISSION) {
+            if (allPermissionsGranted()) {
+                tv.post(this::startCamera);
+            } else {
+                Toast.makeText(this,
+                        "Permissions not granted by the user.",
+                        Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+    private boolean allPermissionsGranted() {
+        for (String permission : REQUIRED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
     }
 }
