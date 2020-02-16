22f68faf04b14efc2bb9bc4bbdb9ba53f79ce9e2
Asmaa Mirkhan <asmaamirkhan.am@gmail.com>
2020-02-16 21:21:33 +0300

    Setting camera's functionality 🤹‍♀️

---
+++ app/src/main/java/com/asmaamir/mlkitdemo/ImageClassificationCustomModel/ImageClassificationAnalyzer.java
@@ -0,0 +1,87 @@
+package com.asmaamir.mlkitdemo.ImageClassificationCustomModel;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.TextureView;
+import android.widget.ImageView;
+
+import androidx.camera.core.CameraX;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageProxy;
+
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetector;
+
+public class ImageClassificationAnalyzer implements ImageAnalysis.Analyzer {
+    private static final String TAG = "MLKitFacesAnalyzer";
+    private FirebaseVisionFaceDetector faceDetector;
+    private TextureView tv;
+    private ImageView iv;
+    private Bitmap bitmap;
+    private Canvas canvas;
+    private Paint dotPaint;
+    private float widthScaleFactor = 1.0f;
+    private float heightScaleFactor = 1.0f;
+    private FirebaseVisionImage fbImage;
+    private CameraX.LensFacing lens;
+
+    ImageClassificationAnalyzer(TextureView tv, ImageView iv, CameraX.LensFacing lens) {
+        this.tv = tv;
+        this.iv = iv;
+        this.lens = lens;
+    }
+
+    @Override
+    public void analyze(ImageProxy image, int rotationDegrees) {
+        if (image == null || image.getImage() == null) {
+            return;
+        }
+        int rotation = degreesToFirebaseRotation(rotationDegrees);
+        fbImage = FirebaseVisionImage.fromMediaImage(image.getImage(), rotation);
+        initDrawingUtils();
+
+    }
+
+    private void initDrawingUtils() {
+        bitmap = Bitmap.createBitmap(tv.getWidth(), tv.getHeight(), Bitmap.Config.ARGB_8888);
+        canvas = new Canvas(bitmap);
+        dotPaint = new Paint();
+        dotPaint.setColor(Color.RED);
+        dotPaint.setStyle(Paint.Style.FILL);
+        dotPaint.setStrokeWidth(2f);
+        dotPaint.setAntiAlias(true);
+        widthScaleFactor = canvas.getWidth() / (fbImage.getBitmap().getWidth() * 1.0f);
+        heightScaleFactor = canvas.getHeight() / (fbImage.getBitmap().getHeight() * 1.0f);
+    }
+
+    private float translateY(float y) {
+        return y * heightScaleFactor;
+    }
+
+    private float translateX(float x) {
+        float scaledX = x * widthScaleFactor;
+        if (lens == CameraX.LensFacing.FRONT) {
+            return canvas.getWidth() - scaledX;
+        } else {
+            return scaledX;
+        }
+    }
+
+    private int degreesToFirebaseRotation(int degrees) {
+        switch (degrees) {
+            case 0:
+                return FirebaseVisionImageMetadata.ROTATION_0;
+            case 90:
+                return FirebaseVisionImageMetadata.ROTATION_90;
+            case 180:
+                return FirebaseVisionImageMetadata.ROTATION_180;
+            case 270:
+                return FirebaseVisionImageMetadata.ROTATION_270;
+            default:
+                throw new IllegalArgumentException("Rotation must be 0, 90, 180, or 270.");
+        }
+    }
+}
